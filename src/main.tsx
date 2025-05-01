2c09ac4df2eb23c9cde896d2d4beb4f29c9fe8ec
Yousef Shanawany <yshanawany@innovationteam.com>
2025-05-01 14:04:25 +0300

    Set up routing and Ably integration
    Integrate react-router-dom for client-side routing.
    Add Ably real-time messaging client and context providers.
    Include placeholder for Ably API key in example env file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AblyProvider, ChannelProvider } from "ably/react";
 
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
+import { ably } from "./services/ably.ts";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
+    <BrowserRouter>
+      <MsalProvider instance={msalInstance}>
+        <AblyProvider client={ably}>
+          <ChannelProvider channelName="slackrosoft-teams">
+            <App />
+          </ChannelProvider>
+        </AblyProvider>
+      </MsalProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
